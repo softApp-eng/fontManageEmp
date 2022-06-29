225e949dd8e190b46075f2abbd176bca9a214bdc
softApp-eng <softmed2018@gmail.com>
2022-06-29 13:53:56 +0100

    insert new data ana comminicate to backEnd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Employee } from './employee.model';
 import { EmpServiceService } from './service/emp-service.service';
 
@@ -17,17 +18,50 @@ export class AppComponent implements OnInit {
         this.getEmployees()
     }
 
- 
+
 
     public getEmployees(): void {
         this.empSrv.getEmp().subscribe((data: Employee[]) => {
             this.employee = data;
             console.log(this.employee)
-            
+
         },
             (error: HttpErrorResponse) => {
                 alert(error.message);
             }
         )
+    }
+
+    public onOpenModel(employee: Employee, mode: string): void {
+        const container = document.getElementById("main-container");
+        const button = document.createElement("button");
+        button.type = "button";
+        button.style.display = "none";
+        button.setAttribute("data-toggle", "modal");
+        if (mode === "add") {
+            button.setAttribute("data-target", "#addEmp");
+        }
+        if (mode === "edit") {
+            button.setAttribute("data-target", "#editEmp");
+        }
+        if (mode === "delete") {
+            button.setAttribute("data-target", "#deleteEmp");
+        }
+
+        container?.appendChild(button);
+        button.click();
+    }
+
+
+    public addNewEmployee(data: NgForm): void {
+
+        this.empSrv.addEmp(data.value).subscribe(
+            (response: Employee) => {
+                console.log(response);
+                document.getElementById("add-employee-form")?.click();
+                this.getEmployees()
+            }, (error: HttpErrorResponse) => {
+                alert(error.error.message)
+            })
     }
 }
